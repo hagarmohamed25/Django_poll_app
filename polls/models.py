d7d8a351f80e7594a0e00d00869197b4394cd606
hagarmohamed25 <hagarmohamed0025@gmail.com>
2026-08-12 16:26:53 +0300

    customize admin and debug add [CU_86cb2w4wc]

--- polls/models.py
+++ polls/models.py
@@ -1,5 +1,5 @@
 import datetime
-
+from django.contrib import admin
 from django.db import models
 from django.utils import timezone
 
@@ -9,6 +9,12 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
+
+    @admin.display(
+            boolean=True,
+            ordering="publish_date",
+            description="published recently?",
+    )
 
     def was_published_recently(self):
         now = timezone.now()
